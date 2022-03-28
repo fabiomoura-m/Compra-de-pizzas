1322ac15623d2127e4096efac2ca3f446707be3c
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-03-28 15:56:53 -0300

    feat: adicionado abertura do modal

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -3,7 +3,7 @@ let pizzaJson = [
     id: 1,
     name: 'Mussarela',
     img: 'assets//images/pizza.png',
-    price: 20.19,
+    price: 40.19,
     sizes: ['100g', '530g', '860g'],
     description:
       'Descrição da pizza em mais de uma linha muito legal bem interessante'
@@ -12,7 +12,7 @@ let pizzaJson = [
     id: 2,
     name: 'Calabresa',
     img: 'assets//images/pizza2.png',
-    price: 18.0,
+    price: 48.0,
     sizes: ['320g', '530g', '860g'],
     description:
       'Descrição da pizza em mais de uma linha muito legal bem interessante'
@@ -21,7 +21,7 @@ let pizzaJson = [
     id: 3,
     name: 'Quatro Queijos',
     img: 'assets//images/pizza3.png',
-    price: 17.45,
+    price: 47.45,
     sizes: ['320g', '530g', '860g'],
     description:
       'Descrição da pizza em mais de uma linha muito legal bem interessante'
@@ -30,7 +30,7 @@ let pizzaJson = [
     id: 4,
     name: 'Americana',
     img: 'assets//images/pizza4.png',
-    price: 19.77,
+    price: 49.77,
     sizes: ['320g', '530g', '860g'],
     description:
       'Descrição da pizza em mais de uma linha muito legal bem interessante'
@@ -39,7 +39,7 @@ let pizzaJson = [
     id: 5,
     name: 'Sorvete',
     img: 'assets//images/pizza5.png',
-    price: 21.43,
+    price: 41.43,
     sizes: ['320g', '530g', '860g'],
     description:
       'Descrição da pizza em mais de uma linha muito legal bem interessante'
@@ -48,7 +48,7 @@ let pizzaJson = [
     id: 6,
     name: 'Moda da Casa',
     img: 'assets//images/pizza6.png',
-    price: 18.55,
+    price: 48.55,
     sizes: ['320g', '530g', '860g'],
     description:
       'Descrição da pizza em mais de uma linha muito legal bem interessante'
@@ -57,7 +57,7 @@ let pizzaJson = [
     id: 7,
     name: 'Chocolate',
     img: 'assets//images/pizza7.png',
-    price: 22.36,
+    price: 42.36,
     sizes: ['320g', '530g', '860g'],
     description:
       'Descrição da pizza em mais de uma linha muito legal bem interessante'
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,19 @@ pizzaJson.map((item, index) => {
   ).innerHTML = ` R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  pizzaItem.querySelector('a').addEventListener('click', e => {
+    // prevenindo a ação padrão, que no caso é recarregamento da página
+    e.preventDefault();
+
+    document.querySelector('.pizzaWindowArea').style.opacity = 0;
+    document.querySelector('.pizzaWindowArea').style.display = 'flex';
+
+    //para que a animação de transition especificada no css possa funcionar
+    setTimeout(() => {
+      document.querySelector('.pizzaWindowArea').style.opacity = 1;
+    }, 200);
+
+    console.log('clicou na pizza');
+  });
 });
