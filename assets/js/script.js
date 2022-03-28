4a3df5df823d4535061941ba306f6e6f4a585453
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-03-28 20:04:32 -0300

    feat: modal preenchido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,50 @@
+const modalQuantidade = 1;
+
 pizzaJson.map((item, index) => {
   let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
 
   document.querySelector('.pizza-area').append(pizzaItem);
 
-  // preencher as informações em pizzaitem
+  // preenchendo as informações em pizzaitem
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
   pizzaItem.querySelector(
     '.pizza-item--price'
   ).innerHTML = ` R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizzaItem.setAttribute('data-key', index); // seta o valor do atributo sendo o index de cada elemento
 
   pizzaItem.querySelector('a').addEventListener('click', e => {
     // prevenindo a ação padrão, que no caso é recarregamento da página
     e.preventDefault();
+    let key = e.target.closest('.pizza-item').getAttribute('data-key'); // pega o elemento mais proximo de "a" que tem a classe .pizza-item e pega o valor do atributo data-key dele.
+    modalQuantidade = 1;
 
+    // mostrando o modal
     document.querySelector('.pizzaWindowArea').style.opacity = 0;
     document.querySelector('.pizzaWindowArea').style.display = 'flex';
-
-    //para que a animação de transition especificada no css possa funcionar
     setTimeout(() => {
       document.querySelector('.pizzaWindowArea').style.opacity = 1;
-    }, 200);
+    }, 200); //para que a animação de transition especificada no css possa funcionar
 
-    console.log('clicou na pizza');
+    //Preenchendo o modal
+    document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
+    document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    document.querySelector('.pizzaInfo--desc').innerHTML =
+      pizzaJson[key].description;
+    document.querySelector(
+      '.pizzaInfo--actualPrice'
+    ).innerHTML = ` R$ ${pizzaJson[key].price.toFixed(2)}`;
+    document
+      .querySelector('.pizzaInfo--size.selected')
+      .classList.remove('selected'); // remove para resetar o tamanho selecionado ao abrir outro modal
+    document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+      if (sizeIndex == 2) {
+        size.classList.add('selected'); // adicionando para sempre que abrir o modal, o tamanho grande estar selecionado
+      }
+      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+    }); //percorre os itens e adiciona os tamanhos
+
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQuantidade;
   });
 });
